9553e36e49e74b3860248e2832bcc5caeb3b0036
orjanhsy <orjanhsy@gmail.com>
2024-07-04 17:50:53 +0200

    add more keywords to token and test lexer

--- monkey/token/token.go
+++ monkey/token/token.go
@@ -37,11 +37,21 @@ const (
 	// Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	RETURN   = "RETURN"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
 )
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"return": RETURN,
+	"if":     IF,
+	"else":   ELSE,
+	"true":   TRUE,
+	"false":  FALSE,
 }
 
 func LookupIdent(ident string) TokenType {
