ce35fab38aa6267b773497b973a6779199c42b41
orjanhsy <orjanhsy@gmail.com>
2024-07-15 19:56:33 +0200

    add test for parsing return statements

--- monkey/parser/parser_test.go
+++ monkey/parser/parser_test.go
@@ -80,3 +80,31 @@ func testLetStatements(t *testing.T, s ast.Statement, name string) bool {
 
 	return true
 }
+
+func TestReturnStatements(t *testing.T) {
+	input := `
+return 5;
+return 10;
+return 993322;
+`
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program should have 3 statements, had %d", len(program.Statements))
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("stmt not *ast.ReturnStatement. got=%T", stmt)
+		}
+
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TokenLiteral was not 'return'. got=%q", returnStmt.TokenLiteral())
+		}
+	}
+}
